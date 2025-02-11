ec4c64db010e9bdf7b6ca9900b57ff10df5119b5
mohittalwar23 <talwarmohit2005@gmail.com>
2025-02-12 03:09:57 +0530

    push

---
+++ sam_bot_nav2_gz/scripts/reach_goal.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python3
+
+
+from geometry_msgs.msg import PoseStamped
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+import rclpy
+from rclpy.duration import Duration
+
+"""
+Basic navigation demo to go to pose.
+"""
+
+
+def main():
+    rclpy.init()
+    navigator = BasicNavigator()
+    navigator.waitUntilNav2Active(localizer="smoother_server")
+
+    # Go to our demos first goal pose
+    goal_pose = PoseStamped()
+    goal_pose.header.frame_id = 'map'
+    goal_pose.header.stamp = navigator.get_clock().now().to_msg()
+    goal_pose.pose.position.x = 0.8
+    goal_pose.pose.position.y = 0.5
+    goal_pose.pose.orientation.w = 1.0
+
+
+    navigator.goToPose(goal_pose)
+
+    i = 0
+    while not navigator.isTaskComplete():
+        ################################################
+        #
+        # Implement some code here for your application!
+        #
+        ################################################
+
+        # Do something with the feedback
+        i = i + 1
+        feedback = navigator.getFeedback()
+
+        if feedback and i % 5 == 0:
+            print('Estimated time of arrival: ' + '{0:.0f}'.format(
+                  Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
+                  + ' seconds.')
+
+            # Some navigation timeout to demo cancellation
+            if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600):
+                navigator.cancelTask()
+
+            
+
+    # Do something depending on the return code
+    result = navigator.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print('Goal succeeded!')
+    elif result == TaskResult.CANCELED:
+        print('Goal was canceled!')
+    elif result == TaskResult.FAILED:
+        print('Goal failed!')
+    else:
+        print('Goal has an invalid return status!')
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
